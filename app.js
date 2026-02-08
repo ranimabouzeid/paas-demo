d7f6d4eecb7faab5824921afed2119022d34cdca
ranimabouzeid <ranim.abouzeid@lau.edu>
2026-02-08 13:26:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("version 2!");
+  res.send("welcome to paas app from aws!");
 });
 
 const port = process.env.PORT || 3000;
