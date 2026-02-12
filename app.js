9cecd410f3dbf212ad2309fee6ad0c9a37d15af4
ranimabouzeid <ranim.abouzeid@lau.edu>
2026-02-12 22:13:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("version 2!");
+  res.send("new version!");
 });
 
 const port = process.env.PORT || 3000;
